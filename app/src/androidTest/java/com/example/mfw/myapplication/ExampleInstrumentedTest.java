25a893853fddff97075f624f6a4744af1b332d68
mfw <mfw0607@163.com>
2019-01-17 11:21:46 +0800

    01001

--- app/src/androidTest/java/com/example/mfw/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mfw/myapplication/ExampleInstrumentedTest.java
@@ -35,4 +35,8 @@ public class ExampleInstrumentedTest {
     public void correct(){
         System.out.println("this is a right test");
     }
+
+    public void correct(){
+        System.out.println("this is a right test");
+    }
 }
